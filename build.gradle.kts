f983dd3e5ebb5edc4d919efb5f72d51ab5a9d102
i30mb1 <fate.i30mb1@gmail.com>
2020-03-26 18:42:25 +0300

    update dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,7 +8,6 @@ buildscript {
     dependencies {
         classpath("com.android.tools.build:gradle:${Versions.gradle}")
         classpath(kotlin("gradle-plugin", version = "1.3.70"))
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.3.70")
         // NOTE: Do not place your application dependencies here; they belong
         // in the individual module build.gradle files
     }
