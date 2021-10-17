b4c8c032b3113bfd932bb19b363976de9ab8aa01
i30mb1 <fate.i30mb1@gmail.com>
2021-10-17 16:31:51 +0300

    add quick bat

---
+++ install-apks.bat
@@ -0,0 +1,10 @@
+@rem BatChat
+@echo off
+cls
+
+rem ================================================
+
+rem del apks.apks
+rem gradlew :app:bundleDebug
+java -jar bundletool.jar build-apks --local-testing --bundle app/build/outputs/bundle/debug/app-debug.aab --output apks.apks
+java -jar bundletool.jar install-apks --apks apks.apks
