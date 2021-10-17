c0564a873804418b92406d1ed8c84cdbce598b6d
i30mb1 <fate.i30mb1@gmail.com>
2021-10-17 22:28:27 +0300

    modify progress bar

--- install-apks.bat
+++ install-apks.bat
@@ -4,7 +4,7 @@ cls
 
 rem ================================================
 
-rem del apks.apks
+del apks.apks
 rem gradlew :app:bundleDebug
 java -jar bundletool.jar build-apks --local-testing --bundle app/build/outputs/bundle/debug/app-debug.aab --output apks.apks
 java -jar bundletool.jar install-apks --apks apks.apks
