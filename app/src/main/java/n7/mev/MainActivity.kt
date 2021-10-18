91c5c6459946d346943f14efca8b1668f69469bb
i30mb1 <fate.i30mb1@gmail.com>
2021-10-18 22:20:59 +0300

    add delay for splash screen

--- app/src/main/java/n7/mev/MainActivity.kt
+++ app/src/main/java/n7/mev/MainActivity.kt
@@ -4,14 +4,23 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.fragment.app.commit
+import androidx.lifecycle.lifecycleScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 import n7.mev.ui.heroes.HeroesFragment
 import n7.mev.ui.sounds.SoundsFragment
 
 class MainActivity : AppCompatActivity(), Navigator {
 
+    private var shouldKeepOnScreen = true
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        installSplashScreen()
+        installSplashScreen().setKeepVisibleCondition(::shouldKeepOnScreen)
+        lifecycleScope.launch {
+            delay(2000)
+            shouldKeepOnScreen = false
+        }
         setContentView(R.layout.activity_main)
 
         if (savedInstanceState == null) {
