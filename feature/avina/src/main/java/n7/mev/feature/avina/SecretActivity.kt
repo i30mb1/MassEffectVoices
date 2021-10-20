a2a5d21d750b8e54fe6aba3fa682b20547e7e533
i30mb1 <fate.i30mb1@gmail.com>
2021-10-20 20:38:58 +0300

    move feature modules to feature folder

---
+++ feature/avina/src/main/java/n7/mev/feature/avina/SecretActivity.kt
@@ -0,0 +1,19 @@
+package n7.mev.feature.avina
+
+import android.content.Context
+import android.os.Bundle
+import androidx.fragment.app.FragmentActivity
+import com.google.android.play.core.splitcompat.SplitCompat
+
+class SecretActivity : FragmentActivity() {
+
+    override fun attachBaseContext(newBase: Context?) {
+        super.attachBaseContext(newBase)
+        SplitCompat.installActivity(this)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_secret)
+    }
+}
